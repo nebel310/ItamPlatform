5b77e6ef5558529a3a2a3b7ea97728ff73832a27
Vlados <89387622+nebel310@users.noreply.github.com>
2024-12-10 22:41:26 +0300

    Docker

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from flask_migrate import Migrate
 from models import db
 from events.routes import events_blueprint
 from auth.routes import auth_blueprint
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/itam_db'
+# Используем переменные окружения для настройки базы данных
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:12345@localhost:5432/itam_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
 migrate = Migrate(app, db)
 
 with app.app_context():
@@ -38,4 +38,4 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, port=5001)
