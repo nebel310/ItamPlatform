40303745018bee72e7e338f9f6f6cca8cc10ff10
Vlados <89387622+nebel310@users.noreply.github.com>
2024-12-10 23:47:24 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -36,4 +36,4 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001, host='0.0.0.0')
